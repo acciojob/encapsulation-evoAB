9d73de0bb413e5e49d641d4fd64edd521a184f37
Abhishek <abhi@gmail.com>
2023-12-13 23:32:33 +0530

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly rw = new RWOnly();
+        rw.name="xyz";
+        System.out.println(rw.name);
         rw.setToken("Abc");
         rw.getToken();
     }
