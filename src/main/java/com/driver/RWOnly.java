1f8283d22ad43703d951212ea020f37df78d61b3
Abhishek <abhi@gmail.com>
2023-12-13 23:21:04 +0530

    Done

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int token;
+
+    public int getToken() {
+        return token;
+    }
+
+    public void setToken(int token) {
+        this.token = token;
+    }
 }
