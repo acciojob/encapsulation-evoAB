af2844302031b44f0dada64c172cebebf771eca5
Abhishek <abhi@gmail.com>
2023-12-13 23:36:21 +0530

    Again

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,9 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly rw = new RWOnly();
-        rw.name="xyz";
-        System.out.println(rw.name);
+//        rw.name="xyz";
+//        System.out.println(rw.name);
+//        java: name has private access in com.driver.RWOnly
         rw.setToken("Abc");
         rw.getToken();
     }
